e1ea72321ea57bb5dee3e6dc4e79193ed2769198
Vanillaa00 <i.zavyalov2006@gmail.com>
2024-07-27 23:54:11 +0400

    Added Jetpack navigation and some changes to navigate between fragments

--- app/src/main/java/com/example/addingnumbers/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/example/addingnumbers/presentation/GameFinishedFragment.kt
@@ -8,22 +8,19 @@ import androidx.activity.OnBackPressedCallback
 import androidx.activity.OnBackPressedDispatcher
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.addingnumbers.R
 import com.example.addingnumbers.databinding.FragmentGameFinishedBinding
 import com.example.addingnumbers.domain.entity.GameResult
 
 class GameFinishedFragment : Fragment() {
 
-    private lateinit var gameResult: GameResult
+    val args by navArgs<GameFinishedFragmentArgs>()
 
     private var _binding: FragmentGameFinishedBinding? = null
     private val binding: FragmentGameFinishedBinding
         get() = _binding ?: throw RuntimeException("FragmentGameFinishedBinding = null")
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        parseArgs()
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,16 +33,11 @@ class GameFinishedFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         onBackPressed()
-        bindViews(gameResult)
+        bindViews()
     }
 
     private fun onBackPressed() {
-        val callback = object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                tryAgain()
-            }
-        }
-        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
+        tryAgain()
     }
 
     override fun onDestroyView() {
@@ -53,20 +45,11 @@ class GameFinishedFragment : Fragment() {
         _binding = null
     }
 
-    private fun parseArgs() {
-        requireArguments().getParcelable<GameResult>(KEY_GAME_RESULT)?.let {
-            gameResult = it
-        }
-    }
-
     private fun tryAgain(){
-        requireActivity().supportFragmentManager.popBackStack(
-            GameFragment.NAME,
-            FragmentManager.POP_BACK_STACK_INCLUSIVE
-        )
+        findNavController().popBackStack()
     }
 
-    private fun bindViews(gameResult: GameResult) {
+    private fun bindViews() {
         with(binding) {
             buttonTryAgain.setOnClickListener {
                 tryAgain()
@@ -74,15 +57,15 @@ class GameFinishedFragment : Fragment() {
             emojiResult.setImageResource(getSmileResId())
             tvQuantityOfRightAnswers.text = String.format(
                 getString(R.string.quantity_of_answers_u_need),
-                gameResult.gameSettings.minCountOfRightAnswers.toString()
+                args.gameResult.gameSettings.minCountOfRightAnswers.toString()
             )
             tvScore.text = String.format(
                 getString(R.string.score),
-                gameResult.countOfRightAnswers.toString()
+                args.gameResult.countOfRightAnswers.toString()
             )
             tvQuantityOfPercents.text = String.format(
                 getString(R.string.percent_of_right_answers_u_need),
-                gameResult.gameSettings.minPercentOfRightAnswers.toString()
+                args.gameResult.gameSettings.minPercentOfRightAnswers.toString()
             )
             tvScorePercent.text = String.format(
                 getString(R.string.score_in_percent),
@@ -91,7 +74,7 @@ class GameFinishedFragment : Fragment() {
         }
     }
 
-    private fun getPercentOfRightAnswers() = with(gameResult){
+    private fun getPercentOfRightAnswers() = with(args.gameResult){
         if (countOfRightAnswers == 0) {
             0
         } else {
@@ -100,23 +83,10 @@ class GameFinishedFragment : Fragment() {
     }
 
     private fun getSmileResId(): Int {
-        return if (gameResult.winner) {
+        return if (args.gameResult.winner) {
             R.drawable.ic_smile
         } else {
             R.drawable.ic_sad
-        }
-    }
-
-    companion object {
-
-        private const val KEY_GAME_RESULT = "game_result"
-
-        fun newInstance(gameResult: GameResult): GameFinishedFragment {
-            return GameFinishedFragment().apply {
-                arguments = Bundle().apply {
-                    putParcelable(KEY_GAME_RESULT, gameResult)
-                }
-            }
         }
     }
 }
